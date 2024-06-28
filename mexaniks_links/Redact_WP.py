494b5c660c77b070a053e4d8a279394315221215
antondanv <antondanilov228@yandex.ru>
2024-06-28 16:35:25 +0500

    Финальная версия 1.0

--- mexaniks_links/Redact_WP.py
+++ mexaniks_links/Redact_WP.py
@@ -1,27 +1,115 @@
+# import requests
+#
+# # Адрес вашего сайта WordPress и путь к REST API
+# base_url = 'http://example.com/wp-json/wp/v2/posts/'
+# post_id = 123  # ID поста, который вы хотите отредактировать
+#
+# # Заголовки для авторизации и установки типа контента
+# headers = {
+#     'Authorization': 'Bearer ваш_токен_доступа',  # Или 'Authorization': 'Basic base64_строка_логин:пароль'
+#     'Content-Type': 'application/json',
+# }
+#
+# # Новые данные для поста
+# new_post_data = {
+#     'title': 'Новый заголовок поста',
+#     'content': 'Новый текст поста',
+# }
+#
+# # Отправка PATCH запроса для редактирования поста
+# response = requests.patch(base_url + str(post_id), headers=headers, json=new_post_data)
+#
+# # Проверка успешности запроса
+# if response.status_code == 200:
+#     print('Пост успешно отредактирован.')
+# else:
+#     print('Ошибка при редактировании поста:', response.status_code)
+#     print(response.text)
 import requests
+import re
+from bs4 import BeautifulSoup
+from colorama import init, Fore
+import json
 
-# Адрес вашего сайта WordPress и путь к REST API
-base_url = 'http://example.com/wp-json/wp/v2/posts/'
-post_id = 123  # ID поста, который вы хотите отредактировать
+def posting(links_to_replace, post_id, count):
+    # Функция для получения содержимого поста по id
+    def get_post_content(post_id, base_url, headers):
+        url = f"{base_url}/wp-json/wp/v2/posts/{post_id}"
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            post_data = response.json()
+            return post_data.get('content', {}).get('rendered', '')
+        else:
+            print(f"Failed to retrieve post (ID: {post_id}). Status code: {response.status_code}")
+            return None
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL21leGFuaWtzLnJ1IiwiaWF0IjoxNzE5NTU5OTE3LCJuYmYiOjE3MTk1NTk5MTcsImV4cCI6MTcyMDE2NDcxNywiZGF0YSI6eyJ1c2VyIjp7ImlkIjoiMSJ9fX0.zWRRKXbJCb5OnJipKcfBkkKPFKl1fYsG_2K2REtSgL0"
+    def check_token_validity(token):
+        headers = {
+            'Authorization': f'Bearer {token}',
+            'Content-Type': 'application/json',
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'
+        }
+        response = requests.get('https://mexaniks.ru/wp-json/wp/v1', headers=headers)
 
-# Заголовки для авторизации и установки типа контента
-headers = {
-    'Authorization': 'Bearer ваш_токен_доступа',  # Или 'Authorization': 'Basic base64_строка_логин:пароль'
-    'Content-Type': 'application/json',
-}
+        if response.status_code in (401, 403, 400):  # Обычно 401 код говорит о проблемах с аутентификацией
+            print("Токен недействителен. Пожалуйста, поменяйте токен.")
+            exit()
+        else:
+            pass
 
-# Новые данные для поста
-new_post_data = {
-    'title': 'Новый заголовок поста',
-    'content': 'Новый текст поста',
-}
 
-# Отправка PATCH запроса для редактирования поста
-response = requests.patch(base_url + str(post_id), headers=headers, json=new_post_data)
+    # Функция для замены ссылок в содержимом поста
+    # Функция для замены ссылок в содержимом поста с использованием BeautifulSoup
+    def replace_links_in_post(post_content, links_to_replace):
+        soup = BeautifulSoup(post_content, 'html.parser')
+        for text, new_url in links_to_replace.items():
+            # Находим все теги <a> с указанным текстом
+            for tag in soup.find_all('a', text=text):
+                # Заменяем значение атрибута href
+                tag['href'] = new_url
+        return str(soup)
 
-# Проверка успешности запроса
-if response.status_code == 200:
-    print('Пост успешно отредактирован.')
-else:
-    print('Ошибка при редактировании поста:', response.status_code)
-    print(response.text)
+
+
+    # Настройки вашего WordPress сайта
+    base_url = 'https://mexaniks.ru'
+     # ID вашего поста, который нужно обновить
+
+    # Заголовки для авторизации или любой другой необходимой аутентификации
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'
+        # Добавьте сюда любые другие заголовки, необходимые для аутентификации или авторизации
+    }
+
+    # Словарь с текстами и новыми ссылками для замены
+    # links_to_replace = {
+    #     "животноводстве": "https://mexaniks.ru/kakie-sushhestvuyut-vidy-selskohozyajstvennyh-uslug/",
+    #     "разведению коз": "https://mexaniks.ru/kakie-sushhestvuyut-vidy-selskohozyajstvennyh-uslug/",
+    #     "соевых бобах": "https://mexaniks.ru/kakie-sushhestvuyut",
+    #     "окунь": "https://mexaniks.ru/kakie-sushhestvuyut-vidy",
+    #     "сома": "https://mexaniks.ru/kakie-kultury-vyrashhivayut-v-afrike/",
+    # Получаем содержимое поста
+    check_token_validity(token)
+    post_content = get_post_content(post_id, base_url, headers)
+    if post_content:
+        # Заменяем ссылки в содержимом поста
+        updated_post_content = replace_links_in_post(post_content, links_to_replace)
+        # Новые данные для поста
+        new_post_data = {
+            'content': updated_post_content,
+        }
+        url = f"{base_url}/wp-json/wp/v2/posts/{post_id}"
+        # Отправка PATCH запроса для редактирования поста
+        response = requests.patch(url, headers=headers, json=new_post_data)
+
+        # Проверка успешности запроса
+        if response.status_code == 200:
+            result_request = json.loads(response.text)
+            print(Fore.GREEN + f'Пост {count} успешно отредактирован.', result_request["link"])
+        else:
+            print('Ошибка при редактировании поста:', response.status_code)
+            print(response.text)
+    else:
+        print(f"Failed to get content for post ID {post_id}")
--- mexaniks_links/perelinks.py
+++ mexaniks_links/perelinks.py
@@ -4,79 +4,223 @@ import pandas as pd
 import time
 import random
 import pymorphy3
+import csv
+from Redact_WP import posting
 
-def open_file(filename):
-    df = pd.read_csv(filename, sep=';', encoding='utf-8', na_filter=False)
-    link = df.loc[0, 'link']
-    return link
+def open_file_title(filename_title):
+    try:
+        # Читаем CSV-файл с помощью Pandas
+        df = pd.read_csv(filename_title, delimiter=';', encoding='utf-8')
 
-def search_word(phrase):
+        # Создаем словарь из двух столбцов
+        dictionary_title = dict(zip(df['title'], df['link']))
 
-    article_titles = [
-        "Как выбрать хороший смартфон",
-        "Подробное руководство по выбору ноутбука",
-        "Секреты эффективного использования Python"
-    ]
+        # Выводим полученный словарь
+        # print(dictionary)
 
-    # Функция для получения всех форм слова с использованием pymorphy2
-    morph = pymorphy3.MorphAnalyzer()
+    except pd.errors.EmptyDataError:
+        print(f"Ошибка: файл {filename_title} пустой или не содержит данных.")
+    except FileNotFoundError:
+        print(f"Ошибка: файл {filename_title} не найден.")
+    except Exception as e:
+        print(f"Произошла ошибка при чтении файла {filename_title}: {e}")
+    return dictionary_title
 
-    # Функция для получения всех форм слов в словосочетании
-    def get_word_forms(phrase):
-        words = phrase.split()
-        all_forms = set()
-        for word in words:
-            parsed_word = morph.parse(word)[0]
-            forms = {parsed_word.normal_form}
-            forms.update({f.word for f in parsed_word.lexeme})
-            all_forms.update(forms)
-        return all_forms
+def open_file_keywords(filename_keywords):
+    try:
+        # Читаем CSV-файл с помощью Pandas
+        df = pd.read_csv(filename_keywords, delimiter=';', encoding='utf-8')
 
-    # Функция для поиска словосочетания и его форм в заголовках статей
-    def find_article_by_phrase(phrase, article_titles):
-        phrase_forms = get_word_forms(phrase)
-        for title in article_titles:
-            normalized_title = title.lower()  # Приводим к нижнему регистру для удобства сравнения
-            found = False
-            for form in phrase_forms:
-                if form in normalized_title:
-                    found = True
-                    break
-            if found:
-                return title
-        return None
+        # Создаем словарь из двух столбцов
+        dictionary_keywords = dict(zip(df['id'], df['keywords']))
 
-    # Пример использования:
-    article_to_link = find_article_by_phrase(phrase, article_titles)
-    if article_to_link:
-        print(f"Для слова '{phrase}' подходит статья '{article_to_link}'")
-    else:
-        print(f"Не удалось найти подходящую статью для словосочетания '{phrase}'")
+        # Выводим полученный словарь
 
-search_word("смартфоны")
 
-def main(link):
-    driver = webdriver.Chrome()
-    driver.get("https://mexaniks.ru/chto-takoe-med/")
-    tag_a = driver.find_elements(By.XPATH, '//header[@class="entry-header "]/following-sibling::div//a')
-    spisok_word = []
+    except pd.errors.EmptyDataError:
+        print(f"Ошибка: файл {filename_keywords} пустой или не содержит данных.")
+    except FileNotFoundError:
+        print(f"Ошибка: файл {filename_keywords} не найден.")
+    except Exception as e:
+        print(f"Произошла ошибка при чтении файла {filename_keywords}: {e}")
+    return dictionary_keywords
 
-    for i in tag_a:
-        link_a = i.get_attribute("href")
-        if link_a == "https://mexaniks.ru/":
-            text = i.text
-            spisok_word.append(text)
+def search_word(filename_title, filename_keywords):
 
-    if not spisok_word:
-        print("Слова не найдены")
-    else:
-        print(spisok_word)
+    # article_titles = [
+    #     "Как выбрать хороший смартфон",
+    #     "Подробное руководство по выбору ноутбука",
+    #     "Секреты эффективного использования Python"
+    # ]
+    dictionary_title = open_file_title(filename_title)
+    dictionary_keywords = open_file_keywords(filename_keywords)
 
-    for word in spisok_word:
-        driver.find_element(By.CLASS_NAME, "wp-block-search__input").click()
-        for char in word:
-            driver.find_element(By.CLASS_NAME, "wp-block-search__input").send_keys(char)
-            time.sleep(random.uniform(0.005, 0.20))
-        time.sleep(100)
+    # Получаем все заголовки статей из словаря
+    article_titles = list(dictionary_title.keys())
+    phrase_all = list(dictionary_keywords.values())
 
+    def get_key(d, value):
+        for k, v in d.items():
+            if v == value:
+                return k
+    for phrase_str in phrase_all:
+        slova = {}
+        phrase_id = get_key(dictionary_keywords, phrase_str)
+        phrase_list = phrase_str.split(",")
+        count = 0
+        for phrase in phrase_list:
+            count += 1
+            #Функция для получения всех форм слова с использованием pymorphy2
+            morph = pymorphy3.MorphAnalyzer()
+            # Функция для получения всех форм слов в словосочетании
+            def get_word_forms(phrase):
+                words = phrase.split()
+                all_forms = set()
+                for word in words:
+                    parsed_word = morph.parse(word)[0]
+                    forms = {parsed_word.normal_form}
+                    forms.update({f.word for f in parsed_word.lexeme})
+                    all_forms.update(forms)
+                return all_forms
+
+            # Функция для поиска словосочетания и его форм в заголовках статей
+            def find_article_by_phrase(phrase, article_titles):
+                phrase_forms = get_word_forms(phrase)
+                for title in article_titles:
+                    normalized_title = title.lower()  # Приводим к нижнему регистру для удобства сравнения
+                    found = False
+                    for form in phrase_forms:
+                        if form in normalized_title:
+                            found = True
+                            break
+                    if found:
+                        return title
+                return None
+
+            # Пример использования:
+            article_to_link = find_article_by_phrase(phrase, article_titles)
+            if len(phrase.split(" ")) == 1:
+                pass
+            else:
+                with open('keyword_question_mexaniks.csv', 'a', encoding='utf-8', newline='') as f:
+                    writer = csv.writer(f, delimiter=';')
+                    writer.writerow(
+                        [phrase_id, phrase])
+            if article_to_link:
+                print(f"Для слова '{phrase}' подходит статья '{article_to_link}'")
+                slova[phrase] = dictionary_title[article_to_link]
+            else:
+                print(f"Не удалось найти подходящую статью для словосочетания '{phrase}'")
+                with open('keyword_mexaniks.csv', 'a', encoding='utf-8', newline='') as f:
+                    writer = csv.writer(f, delimiter=';')
+                    writer.writerow([phrase_id, phrase])
+        posting(slova, phrase_id, count)
+
+
+search_word('Title_Link.csv', 'ID_key.csv')
+#
+#
+# def main(link):
+#     driver = webdriver.Chrome()
+#     driver.get("https://mexaniks.ru/chto-takoe-med/")
+#     tag_a = driver.find_elements(By.XPATH, '//header[@class="entry-header "]/following-sibling::div//a')
+#     spisok_word = []
+#
+#     for i in tag_a:
+#         link_a = i.get_attribute("href")
+#         if link_a == "https://mexaniks.ru/":
+#             text = i.text
+#             spisok_word.append(text)
+#
+#     if not spisok_word:
+#         print("Слова не найдены")
+#     else:
+#         print(spisok_word)
+#
+#     for word in spisok_word:
+#         driver.find_element(By.CLASS_NAME, "wp-block-search__input").click()
+#         for char in word:
+#             driver.find_element(By.CLASS_NAME, "wp-block-search__input").send_keys(char)
+#             time.sleep(random.uniform(0.005, 0.20))
+#         time.sleep(100)
+# def open_file_title(filename_title):
+#     try:
+#         df = pd.read_csv(filename_title, delimiter=';', encoding='utf-8')
+#         dictionary_title = dict(zip(df['title'], df['link']))
+#         return dictionary_title
+#     except pd.errors.EmptyDataError:
+#         print(f"Ошибка: файл {filename_title} пустой или не содержит данных.")
+#     except FileNotFoundError:
+#         print(f"Ошибка: файл {filename_title} не найден.")
+#     except Exception as e:
+#         print(f"Произошла ошибка при чтении файла {filename_title}: {e}")
+#     return {}
+#
+#
+# def open_file_keywords(filename_keywords):
+#     try:
+#         df = pd.read_csv(filename_keywords, delimiter=';', encoding='utf-8')
+#         dictionary_keywords = dict(zip(df['id'], df['keywords']))
+#         return dictionary_keywords
+#     except pd.errors.EmptyDataError:
+#         print(f"Ошибка: файл {filename_keywords} пустой или не содержит данных.")
+#     except FileNotFoundError:
+#         print(f"Ошибка: файл {filename_keywords} не найден.")
+#     except Exception as e:
+#         print(f"Произошла ошибка при чтении файла {filename_keywords}: {e}")
+#     return {}
+#
+#
+# def search_word(phrase, filename_title, filename_keywords):
+#     try:
+#         # Читаем заголовки и ключевые слова из файлов
+#         dictionary_title = open_file_title(filename_title)
+#         dictionary_keywords = open_file_keywords(filename_keywords)
+#
+#         # Получаем все заголовки статей из словаря
+#         article_titles = list(dictionary_title.keys())
+#
+#         # Функция для получения всех форм слова с использованием pymorphy2
+#         morph = pymorphy3.MorphAnalyzer()
+#
+#         # Функция для получения всех форм слов в словосочетании
+#         def get_word_forms(phrase):
+#             words = phrase.split()
+#             all_forms = set()
+#             for word in words:
+#                 parsed_word = morph.parse(word)[0]
+#                 forms = {parsed_word.normal_form}
+#                 forms.update({f.word for f in parsed_word.lexeme})
+#                 all_forms.update(forms)
+#             return all_forms
+#
+#         # Функция для поиска словосочетания и его форм в заголовках статей
+#         def find_article_by_phrase(phrase, article_titles):
+#             phrase_forms = get_word_forms(phrase)
+#             for title in article_titles:
+#                 normalized_title = title.lower()  # Приводим к нижнему регистру для удобства сравнения
+#                 found = False
+#                 for form in phrase_forms:
+#                     if form in normalized_title:
+#                         found = True
+#                         break
+#                 if found:
+#                     return title
+#             return None
+#
+#         # Пример использования:
+#         article_to_link = find_article_by_phrase(phrase, article_titles)
+#         if article_to_link:
+#             link = dictionary_title.get(article_to_link)
+#             keywords = dictionary_keywords.get(link)
+#             print(f"Для слова '{phrase}' подходит статья '{article_to_link}' с ключевыми словами: {keywords}")
+#         else:
+#             print(f"Не удалось найти подходящую статью для словосочетания '{phrase}'")
+#
+#     except Exception as e:
+#         print(f"Произошла ошибка: {e}")
+#
+#
+# # Пример использования функции search_word с указанием файлов
+# search_word("эффективное использование Python", "titles.csv", "keywords.csv")
 
